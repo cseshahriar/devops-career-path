1170d6c7496174df5cc4b627acc2b450e5bedb30
cseshahriar <cse.shahriar.hosen@gmail.com>
2025-01-20 09:23:39 +0600

    multi platform build

--- module-7-docker-cicd/README.md
+++
@@ -1,2 +0,0 @@
-# fastapi-sample-project
-A repository with fastapi to practice tools like docker, ansible, jenkins, gitaction, and kubernetes
--- module-7-docker-cicd/class-1/app/main.py
+++ module-7-docker-cicd/class-1/app/main.py
@@ -8,17 +8,21 @@ collection = db["task_results"]
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
     return {"message": "FastAPI with Celery and Docker Compose"}
+
 
 @app.post("/tasks/{data}")
 def run_task(data: str):
     task = create_task.delay(data)
     return {"task_id": task.id, "status": "Processing"}
 
+
 @app.get("/tasks/")
 def get_all_tasks():
     tasks = list(collection.find({}, {"_id": 0}))
     return {"tasks": tasks}
 
+# some change for docker rebuild
