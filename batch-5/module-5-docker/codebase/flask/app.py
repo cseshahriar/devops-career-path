72afc9186a858fa94fe5d7f1db173ebbd72bbb97
cseshahriar <cse.shahriar.hosen@gmail.com>
2025-11-14 22:27:44 +0600

    docker build

--- batch-5/module-5-docker/codebase/flask/app.py
+++ batch-5/module-5-docker/codebase/flask/app.py
@@ -3,16 +3,21 @@ import os
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
+    ''' Home '''
     return jsonify({
         "message": "Hello from Docker!",
         "environment": os.getenv('ENV', 'development')
     })
 
+
 @app.route('/health')
 def health():
+    ''' health '''
     return jsonify({"status": "healthy"})
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
